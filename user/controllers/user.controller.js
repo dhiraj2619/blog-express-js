8ab696dc1551110fc9b45f703ede4304e7c704e7
Dhiraj <dhirajmymodak22@gmail.com>
2024-08-10 14:24:51 +0530

    pagination added in user page

--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -3,8 +3,19 @@ const { Blog } = require("../../admin/models/blog.model");
 const UserController = {
     getUserBlog: async (req, res) => {
         try {
-            const allblogs = await Blog.findAll();
-            res.render('user/userblog', { allblogs });
+           
+            const page = parseInt(req.query.page) || 1;
+            const limit = 3;
+
+            const offset = (page - 1) * limit;
+            const { count, rows: allblogs } = await Blog.findAndCountAll({
+                limit: limit,
+                offset: offset,
+                order: [['date', 'DESC']],
+            });
+            const totalPages = Math.ceil(count / limit);
+            res.render('user/userblog', { allblogs,  currentPage: page,  
+                totalPages  });
         } catch (error) {
             console.error("unable to get blog page");
             res.status(500).json({ message: "Internal server error" });
